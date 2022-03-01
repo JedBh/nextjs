1f88c6f467d62d1aca01849decdccf2523abe5b6
JedBh <jbenhod@gmail.com>
2022-03-01 17:41:28 +0200

    first todos

---
+++ reactjs-course/src/App.js
@@ -0,0 +1,13 @@
+import Todo from "./components/Todo";
+
+function App() {
+  return (
+    <div>
+      <Todo text="learn react" />
+      <Todo text="master react" />
+      <Todo text="Finsih NextJs before this month ends" />
+    </div>
+  );
+}
+
+export default App;
---
+++ reactjs-course/src/components/Overlay.js
@@ -0,0 +1,5 @@
+function Overlay(props) {
+  return <div className="modal-overlay" onClick={props.onCancel} />;
+}
+
+export default Overlay;
